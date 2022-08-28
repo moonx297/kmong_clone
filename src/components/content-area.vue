1d921cd359967adc371aedc035eebf876d490725
DESKTOP-0OB4BEO\moonx <moonx297@crk.umn.edu>
2022-08-28 16:10:43 +0900

    업데이트2

--- src/components/content-area.vue
+++ src/components/content-area.vue
@@ -11,7 +11,8 @@
 				<div class="card-text">
 					<p class="nickname">nickname</p>
 					<p>로고 제작 공모전 488회우승 로고 제작 드립니다.</p>
-					<strong>10,000</strong>
+					<img class="emoticon" src="@/assets/images/logo.png" />
+					<p class="won">10,000원~</p>
 				</div>
 			</li>
 			<li>
@@ -22,8 +23,9 @@
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">title</p>
+					<p>후기가 증명하고 브랜드의 가치를 담아드립니다.</p>
+					<p class="nickname">10,000</p>
 				</div>
 			</li>
 			<li>
@@ -34,8 +36,8 @@
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">art</p>
+					<p>미술,디자인,작가가 모여 감각적인 로고 제작해드립니다.</p>
 				</div>
 			</li>
 			<li>
@@ -46,344 +48,346 @@
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">design</p>
+					<p>13년 경력 디자인 업체가 로고와 명함서비스 드립니다</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/CI.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">color</p>
+					<p>컬러리스트산업기사,한양대출신디자이너가제작해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/mix.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">logo</p>
+					<p>로고 제작, 정성을 다해 디자인 합니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p26.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">product</p>
+					<p>제품촬영부터 상세페이지까지 올인원으로 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/note.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">simple</p>
+					<p>심플하고 감각적인 로고디자인을 제작해드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/p1.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">all</p>
+					<p>"만족1위" 전단 포스터 리플렛 모든 홍보디자인인쇄OK</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/p2.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">brand</p>
+					<p>브랜드 분위기와 딱 맞는 인쇄물 맞춤 커스텀 명함</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p3.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">painting</p>
+					<p>맞춤형 감성 일러스트를 그려 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/p4.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">simpleisthebest</p>
+					<p>로고 디자인 깔끔하게 해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/p5.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">smartstore</p>
+					<p>
+						스마트스토어, 오픈마켓 상세페이지/배너/이벤트 디자인해 드립니다.
+					</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/p6.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">resonable</p>
+					<p>합리적인 가격으로 잘 팔리는 상세페이지를 제작드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p7.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">detailpage</p>
+					<p>상세페이지, 디자인+제품 촬영+ 기획 한 번에 제작해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/p8.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">diamond</p>
+					<p>다이아몬드처럼 가치있는 로고 및 지원사업 리스트 제공</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/p9.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">modern</p>
+					<p>심플하고 모던한 로고의 차별화를 드립니다</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/p10.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">development</p>
+					<p>브랜드 가치 상승을 위한 디자인을 제안해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p11.png"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">designer</p>
+					<p>해외출신 디자이너의 센스있는 고퀄리티 디자인해 드립니다</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/p12.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">creator</p>
+					<p>크리에이티브한 라벨 패키지 디자인 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/p13.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">banner</p>
+					<p>현수막,배너를 디자인해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/p14.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">museum</p>
+					<p>박물관 7년 경력의 믿을 수 있는 디자이너의 작업물</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p15.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">highquality</p>
+					<p>완성도 높은 고 퀄리티 디자인으로 제작해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/p16.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">needs</p>
+					<p>니즈에 맞추어 정확한 기획아래 모든 인쇄물 디자인해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/p17.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">communication</p>
+					<p>소통을 통해 함께 만들어가는 로고에 감각을 더하다</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/p18.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">cheap</p>
+					<p>세련되고 심플한 로고 빠르고 가성비있게 디자인드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p19.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">whatuneed</p>
+					<p>원하시는 것이 제품이 됩니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/p20.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">article</p>
+					<p>제품의 본질을 이해하는 작품을 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/QbVBu1625654810.jpg"
+						src="@/assets/images/p21.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">feature</p>
+					<p>특징이 잘 드러나는 깔끔하고 세련된 로고디자인</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/blue.jpg"
+						src="@/assets/images/p22.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">background</p>
+					<p>배경컨셉아트, 일러스트 작업해 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/pink.jpg"
+						src="@/assets/images/p23.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">expert</p>
+					<p>전문가가 깔끔한 고퀄리티 상세페이지 만들어 드립니다.</p>
 				</div>
 			</li>
 			<li>
 				<div>
 					<img
-						src="@/assets/images/green.jpg"
+						src="@/assets/images/p24.jpg"
 						style="display: block; margin: 0 auto; width: 100%"
 					/>
 				</div>
 				<div class="card-text">
-					<strong>제목</strong>
-					<p>내용 들어가는 자리입니다.</p>
+					<p class="nickname">click</p>
+					<p>합리적인 가격에 촬영에서 디자인까지 한번에 작업해 드립니다.</p>
 				</div>
 			</li>
 		</ul>
@@ -417,5 +421,16 @@ li:nth-child(5n) {
 	font-size: 12px;
 	color: #999;
 	margin-bottom: 10px;
+}
+.won {
+	font-size: 20px;
+	font-weight: 600;
+	margin-top: 10px;
+	margin-bottom: 10px;
+	float: right;
+}
+.emoticon {
+	width: 20px;
+	/* 이모티콘 금액 바로 왼쪽에 붙게 수정 */
 }
 </style>
